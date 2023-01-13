3ba9e0afe45def9e76e6b5e1707cff422c16b29a
Rebekah <figueroarebekah@gmail.com>
2023-01-12 22:57:42 -0600

    completed getting data for manga cards from api

--- index.js
+++ index.js
@@ -11,12 +11,33 @@ document.addEventListener("DOMContentLoaded", () => {
 		fetch(`https://api.jikan.moe/v4/manga?q=${searchBarInput.value}&sfw`)
 			.then((response) => response.json())
 			.then((json) => {
-				const cardForMangaInfo = document.getElementById("cards-container");
-				cardForMangaInfo.innerHTML = "";
+				const cardsContainer = document.getElementById("cards-container");
+				cardsContainer.innerHTML = "";
 				json.data.forEach((datum) => {
+					console.log(datum);
+					//create card
+					const cardWithMangaInfo = document.createElement("div");
+					// add image
 					const mangaImg = document.createElement("img");
 					mangaImg.src = datum.images.jpg.image_url;
-					cardForMangaInfo.appendChild(mangaImg);
+					//Add title (english)
+					const mangaTitle = document.createElement("h3");
+					mangaTitle.innerHTML = datum.title;
+
+					// add type
+					const mangaType = document.createElement("h4");
+					mangaType.innerHTML = datum.type;
+
+					// Add 'add to library' button
+					const addToLibraryBtn = document.createElement("button");
+					addToLibraryBtn.innerHTML = "Add To Library";
+
+					//Append to card + card container
+					cardsContainer.appendChild(cardWithMangaInfo);
+					cardWithMangaInfo.appendChild(mangaImg);
+					cardWithMangaInfo.appendChild(mangaTitle);
+					cardWithMangaInfo.appendChild(mangaType);
+					cardWithMangaInfo.appendChild(addToLibraryBtn);
 				});
 			});
 	});
