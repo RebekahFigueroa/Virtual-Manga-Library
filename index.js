85780dff6ec90a9b62415eaa80b698e2bc2eca23
Rebekah <figueroarebekah@gmail.com>
2023-02-03 09:50:19 -0600

    added branch without comments

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // #region ****** HEADER CONTAINER- LIBRARY PROFILE DETAILS ****** //
 
-// get a recommendation of unread item from json server and return div container
 const getUnreadMangaRecommendation = () => {
   const recommendationSection = document.createElement("div");
   recommendationSection.classList.add("recommendation-section");
@@ -50,7 +49,6 @@ const getUnreadMangaRecommendation = () => {
   return recommendationSection;
 };
 
-// get count of # of manga series in library and return div container
 const calculateMangaCount = () => {
   const userProfileMangaCount = document.createElement("div");
   userProfileMangaCount.classList.add("user-profile-manga-count");
@@ -66,7 +64,6 @@ const calculateMangaCount = () => {
   return userProfileMangaCount;
 };
 
-// get distribution of manga demographics and return top 3 and return div container
 const calculateMangaDemographic = () => {
   const userProfileMangaType = document.createElement("div");
   userProfileMangaType.classList.add("user-profile-manga-type");
@@ -108,7 +105,6 @@ const calculateMangaDemographic = () => {
   return userProfileMangaType;
 };
 
-// get distribution of manga genres and return top 3 and return div container
 const calculateMangaGenre = () => {
   const userProfileFavoriteGenre = document.createElement("div");
   userProfileFavoriteGenre.classList.add("user-profile-favorite-genre");
@@ -150,7 +146,6 @@ const calculateMangaGenre = () => {
   return userProfileFavoriteGenre;
 };
 
-// get % of library that has been read based on volumes read vs owned and return div container
 const calculatePercentMangaRead = () => {
   const userProfileMangaPercent = document.createElement("div");
   userProfileMangaPercent.classList.add("user-profile-manga-percent");
@@ -178,7 +173,6 @@ const calculatePercentMangaRead = () => {
   return userProfileMangaPercent;
 };
 
-// get libary profile information and return div container
 const libraryProfileInformation = () => {
   const userProfileSection = document.createElement("div");
   userProfileSection.classList.add("user-profile-section");
@@ -187,7 +181,6 @@ const libraryProfileInformation = () => {
   userProfileHeader.classList.add("user-profile-header");
   userProfileHeader.innerHTML = "Fun Stats About The Library";
 
-  // render 4 containers with library stats
   const userProfileMangaCount = calculateMangaCount();
   const userProfileMangaType = calculateMangaDemographic();
   const userProfileFavoriteGenre = calculateMangaGenre();
@@ -202,22 +195,18 @@ const libraryProfileInformation = () => {
   return userProfileSection;
 };
 
-//pull banner, library profile, and recommendation section together to build header
 const createUserProfile = () => {
   const profileContainer = document.getElementById("user-profile-container");
   profileContainer.innerHTML = "";
 
-  // add recommendation section
   const recommendationSection = getUnreadMangaRecommendation();
 
-  // add user profile section
   const userProfileSection = libraryProfileInformation();
 
   const welcomeBanner = document.createElement("h1");
   welcomeBanner.classList.add("welcome-banner");
   welcomeBanner.innerHTML = `Welcome to My Virtual Manga Shelf!`;
 
-  //append
   profileContainer.appendChild(welcomeBanner);
   profileContainer.appendChild(userProfileSection);
   profileContainer.appendChild(recommendationSection);
@@ -226,7 +215,6 @@ const createUserProfile = () => {
 //#endregion
 
 // #region ****** TOGGLE CONTAINER- SWITCH BETWEEN LIBRARY AND SEARCH SECTIONS ****** //
-// tab toggling
 const tabToggle = () => {
   const mangaLibraryTab = document.getElementById("manga-library-tab");
   const searchTab = document.getElementById("search-tab");
@@ -255,7 +243,6 @@ const tabToggle = () => {
 
 // #region ****** MANGA LIBRARY CONTAINER- ALL LIBRARY SECTIONS HERE ****** //
 
-//creating sort methods for library dropdown
 const sortMethods = {
   id: (mangaResult1, mangaResult2) => mangaResult1.id - mangaResult2.id,
   alphabetical: (mangaResult1, mangaResult2) =>
@@ -267,11 +254,9 @@ const sortMethods = {
     new Date(mangaResult2.publishing.split("to")[0]),
 };
 
-// add search bar logic for library
 const searchLibraryLogic = (searchInput) => {
   const mangaCards = document.querySelectorAll(".manga-library-card");
 
-  // Loop through each card to find if text input is within card
   for (var i = 0; i < mangaCards.length; i++) {
     if (
       mangaCards[i].innerText.toLowerCase().includes(searchInput.toLowerCase())
@@ -282,16 +267,13 @@ const searchLibraryLogic = (searchInput) => {
     }
   }
 };
-// containers for search bar/ sorting dropdown - event listeners here
 const mangaLibrary = () => {
-  //default sort behavior for dropdown
   renderLibraryCards("id");
 
   const mangaLibraryDropdown = document.getElementById(
     "manga-library-sort-dropdown"
   );
 
-  // selects new sorting behavior for dropdown
   mangaLibraryDropdown.addEventListener("change", (event) => {
     renderLibraryCards(event.target.value);
   });
@@ -300,59 +282,47 @@ const mangaLibrary = () => {
     "manga-library-search-bar"
   );
 
-  // adds sorting behavior for search bar
   librarySearchInput.addEventListener("change", (event) =>
     searchLibraryLogic(event.target.value)
   );
 };
 
-// make a single library card (includes card creation, hover effects, and delete)
 const mangaLibraryCardCreation = (mangaResult) => {
   const mangaLibraryCardContainer = document.getElementById(
     "manga-library-card-container"
   );
-  //create card (render results in local db first incase you are offline)
   const cardWithMangaInfo = document.createElement("div");
   cardWithMangaInfo.classList.add("manga-library-card");
-  // add image
   const mangaImg = document.createElement("img");
   mangaImg.classList.add("manga-library-image");
   mangaImg.src = mangaResult.image;
-  //Add title (english)
   const mangaTitle = document.createElement("h3");
   mangaTitle.style.textAlign = "center";
   mangaTitle.innerHTML = mangaResult["titleEnglish"];
-  //Add authors
   const mangaAuthors = document.createElement("div");
   mangaAuthors.innerHTML =
     '<span style="font-weight:700;">Authors:</span> ' +
     mangaResult.authors.join(" - ");
-  //Add published info
   const mangaPublished = document.createElement("div");
   mangaPublished.innerHTML =
     '<span style="font-weight:700;">Published:</span> ' +
     mangaResult.publishing;
-  //Add type info
   const mangaType = document.createElement("div");
   mangaType.innerHTML =
     '<span style="font-weight:700;">Type:</span> ' + mangaResult.type;
-  // Add demographics
   const mangaDemographics = document.createElement("div");
   mangaDemographics.innerHTML =
     '<span style="font-weight:700;">Demographic:</span> ' +
     mangaResult.demographic.join(", ");
-  //Add themes
   const mangaThemes = document.createElement("div");
   mangaThemes.innerHTML =
     '<span style="font-weight:700;">Themes:</span> ' +
     mangaResult.themes.join(", ");
-  //Add genres
   const mangaGenres = document.createElement("div");
   mangaGenres.innerHTML =
     '<span style="font-weight:700;">Genres:</span> ' +
     mangaResult.genres.join(", ");
 
-  // add personal scores/ details
   const mangaPersonalDetailsContainer = document.createElement("div");
   mangaPersonalDetailsContainer.classList.add(
     "manga-personal-details-container"
@@ -384,7 +354,6 @@ const mangaLibraryCardCreation = (mangaResult) => {
   mangaScore.classList.add("manga-personal-details-data-score");
   mangaScore.innerHTML = mangaResult.libraryScore;
 
-  //Append to card + card container
   mangaLibraryCardContainer.appendChild(cardWithMangaInfo);
   cardWithMangaInfo.appendChild(mangaImg);
   cardWithMangaInfo.appendChild(mangaTitle);
@@ -402,17 +371,14 @@ const mangaLibraryCardCreation = (mangaResult) => {
   mangaPersonalDetailsContainer.appendChild(mangaScoreTitle);
   mangaPersonalDetailsContainer.appendChild(mangaScore);
 
-  //add hover effect
   cardWithMangaInfo.addEventListener("mouseover", () => {
     cardWithMangaInfo.classList.add("manga-card-hover");
   });
 
-  //remove hover effect
   cardWithMangaInfo.addEventListener("mouseout", () => {
     cardWithMangaInfo.classList.remove("manga-card-hover");
   });
 
-  // remove a card from library
   const removeButton = document.createElement("button");
   removeButton.innerHTML = "Remove Manga";
   removeButton.classList.add("remove-button");
@@ -434,18 +400,15 @@ const mangaLibraryCardCreation = (mangaResult) => {
   return cardWithMangaInfo;
 };
 
-//render all cards for library
 const renderLibraryCards = (sortMethod) => {
   const sortFunction = sortMethods[sortMethod];
   const mangaLibraryCardContainer = document.getElementById(
     "manga-library-card-container"
   );
   mangaLibraryCardContainer.innerHTML = "";
-  //get manga library server from json server
   fetch(`http://localhost:3000/manga`)
     .then((response) => response.json())
     .then((mangaResults) => {
-      // for each result in manga library, make a card and sort by selected method
       mangaResults.sort(sortFunction).forEach((mangaResult) => {
         mangaLibraryCardCreation(mangaResult);
       });
@@ -455,33 +418,25 @@ const renderLibraryCards = (sortMethod) => {
 
 // #region ****** SEARCH CONTAINER- ADD RESULTS TO LIBRARY ****** //
 
-// store results of search API request (aka json.data) to allow toggle saving
 let searchResults = [];
-// create cards for search from Jikan api
 const createSearchCards = () => {
   const cardsContainer = document.getElementById("search-cards-container");
   cardsContainer.innerHTML = "";
   searchResults.forEach((searchResult) => {
-    //create card
     const cardWithMangaInfo = document.createElement("div");
     cardWithMangaInfo.classList.add("manga-card");
-    // add image
     const mangaImg = document.createElement("img");
     mangaImg.classList.add("manga-image");
     mangaImg.src = searchResult.images.jpg.image_url;
-    //Add title (english)
     const mangaTitle = document.createElement("h3");
     mangaTitle.innerHTML = searchResult.title;
 
-    // add type - example: manga or light novel
     const mangaType = document.createElement("h4");
     mangaType.innerHTML = searchResult.type;
 
-    // Add 'add to library' button
     const addToLibraryBtn = document.createElement("button");
     addToLibraryBtn.innerHTML = "Add To Library";
     addToLibraryBtn.classList.add("add-button");
-    // add manga search button behavior to add to library
     addToLibraryBtn.addEventListener("click", () => {
       fetch(`http://localhost:3000/manga`, {
         method: "POST",
@@ -512,39 +467,33 @@ const createSearchCards = () => {
       confirm("Manga was added!");
     });
 
-    //Append to card + card container
     cardsContainer.appendChild(cardWithMangaInfo);
     cardWithMangaInfo.appendChild(mangaImg);
     cardWithMangaInfo.appendChild(mangaTitle);
     cardWithMangaInfo.appendChild(mangaType);
     cardWithMangaInfo.appendChild(addToLibraryBtn);
 
-    //add hover effect
     cardWithMangaInfo.addEventListener("mouseover", () => {
       cardWithMangaInfo.classList.add("manga-card-hover");
     });
 
-    //remove hover effect
     cardWithMangaInfo.addEventListener("mouseout", () => {
       cardWithMangaInfo.classList.remove("manga-card-hover");
     });
   });
 };
 
-//fetch the jikan data and render search result cards onto container
 const renderSearchResultCards = () => {
   const searchBarSubmitButton = document.getElementById(
     "search-bar-submit-btn"
   );
   const searchBarInput = document.getElementById("search-bar");
 
-  // add new manga search results
   searchBarSubmitButton.addEventListener("click", (event) => {
     event.preventDefault();
     fetch(`https://api.jikan.moe/v4/manga?q=${searchBarInput.value}&sfw`)
       .then((response) => response.json())
       .then((json) => {
-        //store search results for later to re-render with toggle
         searchResults = json.data;
         createSearchCards();
       });
@@ -555,18 +504,13 @@ const renderSearchResultCards = () => {
 
 // #region ****** DOM MANIPULATION ****** //
 
-// implment all functions/ implement to DOM here
 document.addEventListener("DOMContentLoaded", () => {
-  //header section
   createUserProfile();
 
-  //toggle behavior for tabs
   tabToggle();
 
-  // render manga library
   mangaLibrary();
 
-  // render search results
   renderSearchResultCards();
 });
 
